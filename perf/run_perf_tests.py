639584352cad4028979d321834d60c38e18d34c1
lupin012 <58134934+lupin012@users.noreply.github.com>
2024-03-19 23:13:36 +0100

    modified SILKRPC into SILKWORM and RPCDAEMON into ERIGON

--- perf/run_perf_tests.py
+++ perf/run_perf_tests.py
@@ -24,8 +24,8 @@ DEFAULT_VEGETA_PATTERN_TAR_FILE = ""
 DEFAULT_DAEMON_VEGETA_ON_CORE = "-:-"
 DEFAULT_ERIGON_ADDRESS = "localhost:9090"
 DEFAULT_ERIGON_BUILD_DIR = ""
-DEFAULT_SILKRPC_BUILD_DIR = ""
-DEFAULT_RPCDAEMON_ADDRESS = "localhost"
+DEFAULT_SILKWORM_BUILD_DIR = ""
+DEFAULT_ERIGON_ADDRESS = "localhost"
 DEFAULT_TEST_MODE = "3"
 DEFAULT_WAITING_TIME = 5
 DEFAULT_MAX_CONN = "9000"
@@ -33,17 +33,17 @@ DEFAULT_TEST_TYPE = "eth_getLogs"
 DEFAULT_VEGETA_RESPONSE_TIMEOUT = "300"
 DEFAULT_MAX_BODY_RSP = "1500"
 
-SILKRPC="silk"
-RPCDAEMON="rpcdaemon"
-SILKRPC_SERVER_NAME="rpcdaemon"
-RPCDAEMON_SERVER_NAME="rpcdaemon"
+SILKWORM="silkworm"
+ERIGON="rpcdaemon"
+SILKWORM_SERVER_NAME="rpcdaemon"
+ERIGON_SERVER_NAME="rpcdaemon"
 RAND_NUM = randint(0, 100000)
 RUN_TEST_DIRNAME = "/tmp/run_tests_" + str(RAND_NUM)
 VEGETA_PATTERN_DIRNAME = RUN_TEST_DIRNAME + "/erigon_stress_test"
 VEGETA_REPORT = RUN_TEST_DIRNAME + "/vegeta_report.hrd"
 VEGETA_TAR_FILE_NAME = RUN_TEST_DIRNAME + "/vegeta_TAR_File"
-VEGETA_PATTERN_SILKRPC_BASE = VEGETA_PATTERN_DIRNAME + "/vegeta_geth_"
-VEGETA_PATTERN_RPCDAEMON_BASE = VEGETA_PATTERN_DIRNAME + "/vegeta_erigon_"
+VEGETA_PATTERN_SILKWORM_BASE = VEGETA_PATTERN_DIRNAME + "/vegeta_geth_"
+VEGETA_PATTERN_ERIGON_BASE = VEGETA_PATTERN_DIRNAME + "/vegeta_erigon_"
 
 def usage(argv):
     """ Print script usage """
@@ -62,14 +62,14 @@ def usage(argv):
     print("-A,--additional-string-name <string>: string to be add in the file name")
     print("-b,--blockchain <chain name>:         mandatory in case of -R or -u")
     print("-y,--test-type <test-type>:           eth_call, eth_getLogs, ...                                         [default: " + DEFAULT_TEST_TYPE + "]")
-    print("-m,--test-mode <0,1,2>:               silkrpc(1), rpcdaemon(2), both(3)                                  [default: " + str(DEFAULT_TEST_MODE) + "]")
+    print("-m,--test-mode <0,1,2>:               silkworm(1), erigon(2), both(3)                                  [default: " + str(DEFAULT_TEST_MODE) + "]")
     print("-p,--pattern-file <file-name>:        path to the request file for Vegeta attack                         [default: " + DEFAULT_VEGETA_PATTERN_TAR_FILE +"]")
     print("-r,--repetitions <number>:            number of repetitions for each element in test sequence (e.g. 10)  [default: " + str(DEFAULT_REPETITIONS) + "]")
     print("-t,--test-sequence <seq>:             list of qps/timeas <qps1>:<t1>,... (e.g. 200:30,400:10)            [default: " + DEFAULT_TEST_SEQUENCE + "]")
     print("-w,--wait-after-test-sequence <secs>: time interval between successive test iterations in sec            [default: " + str(DEFAULT_WAITING_TIME) + "]")
-    print("-d,--rpc-daemon-address <addr>:       address of RPCDaemonc (e.g. 192.2.3.1)                             [default: " + DEFAULT_RPCDAEMON_ADDRESS +"]")
+    print("-d,--rpc-daemon-address <addr>:       address of RPCDaemonc (e.g. 192.2.3.1)                             [default: " + DEFAULT_ERIGON_ADDRESS +"]")
     print("-g,--erigon-dir <path>:               path to erigon folder (e.g. /home/erigon)                          [default: " + DEFAULT_ERIGON_BUILD_DIR + "]")
-    print("-s,--silk-dir <path>:                 path to silk folder (e.g. /home/silkworm)                          [default: " + DEFAULT_SILKRPC_BUILD_DIR + "]")
+    print("-s,--silk-dir <path>:                 path to silk folder (e.g. /home/silkworm)                          [default: " + DEFAULT_SILKWORM_BUILD_DIR + "]")
     print("-c,--run-vegeta-on-core <...>         taskset format for vegeta (e.g. 0-1:2-3 or 0-2:3-4)                [default: " + DEFAULT_DAEMON_VEGETA_ON_CORE +"]")
     print("-T,--response-timeout <timeout>:      vegeta response timeout                                            [default: " + DEFAULT_VEGETA_RESPONSE_TIMEOUT + "]")
     print("-M,--max-body-rsp <size>:             max number of bytes to read from response bodies                   [default: " + DEFAULT_MAX_BODY_RSP + "]")
@@ -92,10 +92,10 @@ class Config:
         self.vegeta_pattern_tar_file = DEFAULT_VEGETA_PATTERN_TAR_FILE
         self.daemon_vegeta_on_core = DEFAULT_DAEMON_VEGETA_ON_CORE
         self.erigon_dir = DEFAULT_ERIGON_BUILD_DIR
-        self.silkrpc_dir = DEFAULT_SILKRPC_BUILD_DIR
+        self.silkworm_dir = DEFAULT_SILKWORM_BUILD_DIR
         self.repetitions = DEFAULT_REPETITIONS
         self.test_sequence = DEFAULT_TEST_SEQUENCE
-        self.rpc_daemon_address = DEFAULT_RPCDAEMON_ADDRESS
+        self.rpc_daemon_address = DEFAULT_ERIGON_ADDRESS
         self.test_mode = DEFAULT_TEST_MODE
         self.test_type = DEFAULT_TEST_TYPE
         self.additional_string = ""
@@ -161,7 +161,7 @@ class Config:
                         print("ERROR: incompatible option -d/rpc-daemon-address with -g/erigon-dir -s/silk-dir")
                         usage(argv)
                     local_config = 1
-                    self.silkrpc_dir = optarg
+                    self.silkworm_dir = optarg
                 elif option in ("-r", "--repetitions"):
                     self.repetitions = int(optarg)
                 elif option in ("-t", "--test-sequence"):
@@ -171,8 +171,8 @@ class Config:
                     if os.path.exists(self.erigon_dir) == 0:
                         print ("ERROR: erigon buildir not specified correctly: ", self.erigon_dir)
                         usage(argv)
-                    if os.path.exists(self.silkrpc_dir) == 0:
-                        print ("ERROR: silkrpc buildir not specified correctly: ", self.silkrpc_dir)
+                    if os.path.exists(self.silkworm_dir) == 0:
+                        print ("ERROR: silkrpc buildir not specified correctly: ", self.silkworm_dir)
                         usage(argv)
                     if specified_chain == 0:
                         print ("ERROR: chain not specified ")
@@ -186,8 +186,8 @@ class Config:
                     if os.path.exists(self.erigon_dir) == 0:
                         print ("ERROR: erigon buildir not specified correctly: ", self.erigon_dir)
                         usage(argv)
-                    if os.path.exists(self.silkrpc_dir) == 0:
-                        print ("ERROR: silkrpc buildir not specified correctly: ", self.silkrpc_dir)
+                    if os.path.exists(self.silkworm_dir) == 0:
+                        print ("ERROR: silkrpc buildir not specified correctly: ", self.silkworm_dir)
                         usage(argv)
                     if specified_chain == 0:
                         print ("ERROR: chain not specified ")
@@ -267,9 +267,9 @@ class PerfTest:
 
         # If address is provided substitute the address and port of daemon in the vegeta file
         if self.config.rpc_daemon_address != "localhost":
-            cmd = "sed -i 's/localhost/" + self.config.rpc_daemon_address + "/g' " + VEGETA_PATTERN_SILKRPC_BASE + self.config.test_type + ".txt"
+            cmd = "sed -i 's/localhost/" + self.config.rpc_daemon_address + "/g' " + VEGETA_PATTERN_SILKWORM_BASE + self.config.test_type + ".txt"
             os.system(cmd)
-            cmd = "sed -i 's/localhost/" + self.config.rpc_daemon_address + "/g' " + VEGETA_PATTERN_RPCDAEMON_BASE + self.config.test_type + ".txt"
+            cmd = "sed -i 's/localhost/" + self.config.rpc_daemon_address + "/g' " + VEGETA_PATTERN_ERIGON_BASE + self.config.test_type + ".txt"
             os.system(cmd)
 
     def execute(self, test_number, name, qps_value, duration):
@@ -279,10 +279,10 @@ class PerfTest:
                 status = os.system("sync && sudo sysctl vm.drop_caches=3 > /dev/null")
             elif sys.platform == "darwin": # OS X
                 status = os.system("sync && sudo purge > /dev/null")
-        if name == SILKRPC:
-            pattern = VEGETA_PATTERN_SILKRPC_BASE + self.config.test_type + ".txt"
+        if name == SILKWORM:
+            pattern = VEGETA_PATTERN_SILKWORM_BASE + self.config.test_type + ".txt"
         else:
-            pattern = VEGETA_PATTERN_RPCDAEMON_BASE + self.config.test_type + ".txt"
+            pattern = VEGETA_PATTERN_ERIGON_BASE + self.config.test_type + ".txt"
         on_core = self.config.daemon_vegeta_on_core.split(':')
         if self.config.max_connection == "0":
             vegeta_cmd = " vegeta attack -keepalive -rate=" + qps_value + " -format=json -duration=" + duration + "s -timeout=" + \
@@ -307,10 +307,10 @@ class PerfTest:
         while 1:
             time.sleep(3)
             if self.config.check_server_alive:
-                if name == SILKRPC:
-                    cmd = "ps aux | grep '" + SILKRPC_SERVER_NAME + "' | grep -v 'grep' | awk '{print $2}'"
+                if name == SILKWORM:
+                    cmd = "ps aux | grep '" + SILKWORM_SERVER_NAME + "' | grep -v 'grep' | awk '{print $2}'"
                 else:
-                    cmd = "ps aux | grep '" + RPCDAEMON_SERVER_NAME + "' | grep -v 'grep' | awk '{print $2}'"
+                    cmd = "ps aux | grep '" + ERIGON_SERVER_NAME + "' | grep -v 'grep' | awk '{print $2}'"
                 pid = os.popen(cmd).read()
                 if pid == "" :
                     # the server is dead; kill vegeta and returns fails
@@ -462,15 +462,17 @@ class TestReport:
         erigon_commit = ""
         silkrpc_commit = ""
         if self.config.test_mode in ("1", "3"):
-            command = "cd " + self.config.silkrpc_dir + " && git rev-parse HEAD"
+            command = "cd " + self.config.silkworm_dir + " && git rev-parse HEAD"
             silkrpc_commit = os.popen(command).read().replace('\n', '')
 
         if self.config.test_mode in ("2", "3"):
             command = "cd " + self.config.erigon_dir + " && git rev-parse HEAD"
             erigon_commit = os.popen(command).read().replace('\n', '')
 
+        print (checksum)
+        print (gcc_vers)
         self.write_test_header(model[1], bogomips, kern_vers, self.config.daemon_vegeta_on_core, self.config.vegeta_pattern_tar_file, 
-                               checksum, gcc_vers, go_vers, silkrpc_commit, erigon_commit) 
+                               checksum[0], gcc_vers[0], go_vers, silkrpc_commit, erigon_commit) 
 
     def write_test_header_on_json(self, model, bogomips, kern_vers, taskset, vegeta_file, checksum, gcc_vers, go_vers, silkrpc_commit, erigon_commit):
         """ Writes test header on json """
@@ -503,7 +505,7 @@ class TestReport:
             self.writer.writerow(["", "", "", "", "", "", "", "", "", "", "", "", "product", product])
         else:
             self.writer.writerow(["", "", "", "", "", "", "", "", "", "", "", "", "board", Hardware.board()])
-        self.writer.writerow(["", "", "", "", "", "", "", "", "", "", "", "", "cpu", model[1]])
+        self.writer.writerow(["", "", "", "", "", "", "", "", "", "", "", "", "cpu", model])
         self.writer.writerow(["", "", "", "", "", "", "", "", "", "", "", "", "bogomips", bogomips])
         self.writer.writerow(["", "", "", "", "", "", "", "", "", "", "", "", "kernel", kern_vers])
         self.writer.writerow(["", "", "", "", "", "", "", "", "", "", "", "", "taskset", taskset])
@@ -515,7 +517,7 @@ class TestReport:
         self.writer.writerow(["", "", "", "", "", "", "", "", "", "", "", "", "erigonVersion", erigon_commit])
         self.writer.writerow([])
         self.writer.writerow([])
-        self.writer.writerow(["Daemon", "TestNo", "Qps", "Time", "Min", "Mean", "50", "90", "95", "99", "Max", "Ratio", "Error"])
+        self.writer.writerow(["Daemon", "TestNo", "Qps", "Time(secs)", "Min", "Mean", "50", "90", "95", "99", "Max", "Ratio", "Error"])
         self.csv_file.flush()
 
         if self.config.json_report:
@@ -524,7 +526,7 @@ class TestReport:
 
     def write_test_report(self, daemon, test_number, threads, qps_value, duration, min_latency, mean, fifty, ninty, nintyfive, nintynine, max_latency, ratio, error):
         """ Writes on CSV the latency data for one completed test """
-        self.writer.writerow([daemon, str(test_number), threads, qps_value, duration, min_latency, mean, fifty, ninty, nintyfive, nintynine, max_latency, ratio, error])
+        self.writer.writerow([daemon, str(test_number), qps_value, duration, min_latency, mean, fifty, ninty, nintyfive, nintynine, max_latency, ratio, error])
         self.csv_file.flush()
 
         if self.config.json_report:
@@ -536,7 +538,7 @@ class TestReport:
             'daemon': daemon,
             'test_number': test_number, 
             'qps': qps_value,
-            'duration': duration,
+            'duration(secs)': duration,
             'min': min_latency,
             'mean': mean,
             '50%': fifty,
@@ -546,7 +548,7 @@ class TestReport:
             'max': max_latency,
             'ratio': ratio, 
             'error': error
-        }
+        })
 
     def close(self):
         """ Close the report """
@@ -574,7 +576,7 @@ def main(argv):
     current_sequence = str(config.test_sequence).split(',')
 
     if config.test_mode in ("1", "3"):
-        result = perf_test.execute_sequence(current_sequence, SILKRPC)
+        result = perf_test.execute_sequence(current_sequence, SILKWORM)
         if result == 0:
             print("Server dead test Aborted!")
             if config.create_test_report:
@@ -584,7 +586,7 @@ def main(argv):
             print("--------------------------------------------------------------------------------------------\n")
 
     if config.test_mode in ("2", "3"):
-        result = perf_test.execute_sequence(current_sequence, RPCDAEMON)
+        result = perf_test.execute_sequence(current_sequence, ERIGON)
         if result == 0:
             print("Server dead test Aborted!")
             if config.create_test_report:
