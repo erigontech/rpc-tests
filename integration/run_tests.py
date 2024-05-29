9efb5d9e9a07289a03c907283d817c072289e32e
Sixtysixter <20945591+Sixtysixter@users.noreply.github.com>
2024-05-29 15:41:52 +0200

    Added trace_callMany API tests on mainnet (#177)

--- integration/run_tests.py
+++ integration/run_tests.py
@@ -100,6 +100,17 @@ tests_not_compared = [
     "mainnet/trace_call/test_16.json",  # diff on callType
     "mainnet/trace_call/test_17.tar",  # diff on callType
 
+    "mainnet/trace_callMany/test_01.json",  # diff on balance
+    "mainnet/trace_callMany/test_02.json",  # diff on balance
+    "mainnet/trace_callMany/test_03.json",  # diff on balance
+    "mainnet/trace_callMany/test_04.json",  # diff on balance
+    "mainnet/trace_callMany/test_05.json",  # diff on balance
+    "mainnet/trace_callMany/test_06.json",  # diff on balance
+    "mainnet/trace_callMany/test_08.json",  # diff on balance
+    "mainnet/trace_callMany/test_09.json",  # diff on balance
+    "mainnet/trace_callMany/test_10.json",  # diff on balance
+    "mainnet/trace_callMany/test_11.json",  # diff on balance
+  
     "mainnet/trace_replayTransaction/test_02.tar",  # diff on gasCost
     "mainnet/trace_replayTransaction/test_03.tar",  # diff on gasCost
     "mainnet/trace_replayTransaction/test_04.tar",  # diff on gasCost
